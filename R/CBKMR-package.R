d9a2a0549a4b38f6610b04b47f82addc260a3767
Anirban Chakraborty <anirban0451@gmail.com>
2025-11-03 00:00:15 -0500

    Add order_maxmin from GpGp package

--- R/CBKMR-package.R
+++ R/CBKMR-package.R
@@ -1,5 +1,6 @@
 ## usethis namespace: start
 #' @importFrom GpGp find_ordered_nn
+#' @importFrom GpGp order_maxmin
 #' @importFrom Rcpp sourceCpp
 #' @useDynLib CBKMR, .registration = TRUE
 ## usethis namespace: end
