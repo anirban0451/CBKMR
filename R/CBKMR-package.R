20569767ed10a1b30908cc33e735473ca43e2bec
Anirban Chakraborty <anirban0451@gmail.com>
2025-11-24 17:42:00 -0500

    Update documentation

--- R/CBKMR-package.R
+++ R/CBKMR-package.R
@@ -2,6 +2,12 @@
 #' @importFrom GpGp find_ordered_nn
 #' @importFrom GpGp order_maxmin
 #' @importFrom Rcpp sourceCpp
+#' @importFrom stats dbeta
+#' @importFrom stats dnorm
+#' @importFrom stats dunif
+#' @importFrom stats qnorm
+#' @importFrom stats rnorm
+#' @importFrom stats runif
 #' @useDynLib CBKMR, .registration = TRUE
 ## usethis namespace: end
 NULL
